52bfd06b049c8f9a7f26f6dd2b910efcdb578ff7
Thai Nguyen <xuanthainguyen23@gmail.com>
2021-11-05 22:36:58 +0700

    Add pie chart

--- Hyperloung/Modules/Charts/DetailChart/aos_mb_pie_01.swift
+++ Hyperloung/Modules/Charts/DetailChart/aos_mb_pie_01.swift
@@ -6,36 +6,136 @@
 //
 
 import UIKit
+import Charts
 
-class aos_mb_pie_01: UIView {
-    private var circleChart: HyperCircleChartView!
+class aos_mb_pie_01: UIView, ChartViewDelegate {
+    private var chartView: PieChartView!
+    let parties = ["Party A", "Party B", "Party C", "Party D", "Party E", "Party F",
+                   "Party G", "Party H", "Party I", "Party J", "Party K", "Party L",
+                   "Party M", "Party N", "Party O", "Party P", "Party Q", "Party R",
+                   "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
+                   "Party Y", "Party Z"]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        circleChart = HyperCircleChartView(frame: bounds)
-        addSubview(circleChart)
-        circleChart.fullscreen()
-        circleChart.datasource = self
-        layoutIfNeeded()
-        DispatchQueue.main.async {
-            self.circleChart.drawChart()
+        updateChartData()
+    }
+    
+    func updateChartData()  {
+        
+        let chart = PieChartView(frame: bounds)
+        let track = ["Income", "Expense", "Wallet", "Bank"]
+        let money = [38, 22, 18, 20]
+        
+        var entries = [PieChartDataEntry]()
+        for (index, value) in money.enumerated() {
+            let entry = PieChartDataEntry()
+            entry.y = Double(value)
+//            entry.label = track[index]
+            entries.append( entry)
         }
+        
+        // 3. chart setup
+        let set = PieChartDataSet( entries: entries, label: "Pie Chart")
+        // this is custom extension method. Download the code for more details.
+        set.drawValuesEnabled = false
+//        set.drawEntryLabelsEnabled = false
+        chart.drawEntryLabelsEnabled = false
+        var colors: [UIColor] = []
+        chart.legend.enabled = false
+        [#colorLiteral(red: 0.2078431373, green: 0.3411764706, blue: 0.7882352941, alpha: 1),#colorLiteral(red: 0.1215686275, green: 0.5725490196, blue: 0.8941176471, alpha: 1),#colorLiteral(red: 0.3803921569, green: 0.8156862745, blue: 0.9411764706, alpha: 1),#colorLiteral(red: 0.2941176471, green: 0.7725490196, blue: 0.7647058824, alpha: 1)].forEach { color in
+            colors.append(color)
+        }
+        
+        set.colors = colors
+        let data = PieChartData(dataSet: set)
+        chart.data = data
+        chart.noDataText = "No data available"
+        // user interaction
+        chart.isUserInteractionEnabled = true
+        
+        chart.centerText = "39.6억"
+        chart.holeRadiusPercent = 0.65
+        
+        chart.transparentCircleColor = UIColor.clear
+        addSubview(chart)
+        chart.fullscreen()
+    }
+    
+    
+    func setDataCount(_ count: Int, range: UInt32) {
+        let entries = (0..<count).map { (i) -> PieChartDataEntry in
+            // IMPORTANT: In a PieChart, no values (Entry) should have the same xIndex (even if from different DataSets), since no values can be drawn above each other.
+            return PieChartDataEntry(value: Double(arc4random_uniform(range) + range / 5),
+                                     label: parties[i % parties.count],
+                                     icon: #imageLiteral(resourceName: "ic_question"))
+        }
+        
+        let set = PieChartDataSet(entries: entries, label: "Election Results")
+        set.drawIconsEnabled = false
+        set.sliceSpace = 2
+        
+        
+        set.colors = ChartColorTemplates.vordiplom()
+            + ChartColorTemplates.joyful()
+            + ChartColorTemplates.colorful()
+            + ChartColorTemplates.liberty()
+            + ChartColorTemplates.pastel()
+            + [UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)]
+        
+        let data = PieChartData(dataSet: set)
+        
+        let pFormatter = NumberFormatter()
+        pFormatter.numberStyle = .percent
+        pFormatter.maximumFractionDigits = 1
+        pFormatter.multiplier = 1
+        pFormatter.percentSymbol = " %"
+        data.setValueFormatter(DefaultValueFormatter(formatter: pFormatter))
+        
+        data.setValueFont(.systemFont(ofSize: 11, weight: .light))
+        data.setValueTextColor(.black)
+        
+        chartView.data = data
+        chartView.highlightValues(nil)
+    }
+    
+    func setup(pieChartView chartView: PieChartView) {
+        chartView.usePercentValuesEnabled = true
+        chartView.drawSlicesUnderHoleEnabled = false
+        chartView.holeRadiusPercent = 0.58
+        chartView.transparentCircleRadiusPercent = 0.61
+        chartView.chartDescription?.enabled = false
+        chartView.setExtraOffsets(left: 5, top: 10, right: 5, bottom: 5)
+        
+        chartView.drawCenterTextEnabled = true
+        
+        let paragraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.lineBreakMode = .byTruncatingTail
+        paragraphStyle.alignment = .center
+        
+        let centerText = NSMutableAttributedString(string: "Charts\nby Daniel Cohen Gindi")
+        centerText.setAttributes([.font : UIFont(name: "HelveticaNeue-Light", size: 13)!,
+                                  .paragraphStyle : paragraphStyle], range: NSRange(location: 0, length: centerText.length))
+        centerText.addAttributes([.font : UIFont(name: "HelveticaNeue-Light", size: 11)!,
+                                  .foregroundColor : UIColor.gray], range: NSRange(location: 10, length: centerText.length - 10))
+        centerText.addAttributes([.font : UIFont(name: "HelveticaNeue-Light", size: 11)!,
+                                  .foregroundColor : UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)], range: NSRange(location: centerText.length - 19, length: 19))
+        chartView.centerAttributedText = centerText;
+        
+        chartView.drawHoleEnabled = true
+        chartView.rotationAngle = 0
+        chartView.rotationEnabled = true
+        chartView.highlightPerTapEnabled = true
+        
+        let l = chartView.legend
+        l.horizontalAlignment = .right
+        l.verticalAlignment = .top
+        l.orientation = .vertical
+        l.drawInside = false
+        l.xEntrySpace = 7
+        l.yEntrySpace = 0
+        l.yOffset = 0
+        //        chartView.legend = l
     }
 }
 
-extension aos_mb_pie_01: HyperCircleDataSource {
-    var internalDescription: (String, UIColor)? {
-        return ("36%", .black)
-    }
-    
-    var lineWidth: CGFloat {
-        return 8
-    }
-    
-    var dataSet: [HyperCircleData] {
-        return [HyperCircleData(percent: 0.3, color: .red)]
-    }
-    
-    func description(of data: HyperCircleData) -> String {
-        return "12억"
-    }
-}
